904f4dfd4a90bb04fc812598f8788d175c8f262e
Francesco Pretto <ceztko@gmail.com>
2021-10-08 15:44:03 +0200

    [FIX] Fixed XCode clang compilation

--- src/pdfmm/base/PdfPredefinedEncoding.h
+++ src/pdfmm/base/PdfPredefinedEncoding.h
@@ -63,7 +63,7 @@ namespace mm
         inline const PdfName& GetName() const { return m_name; }
 
     protected:
-        void getExportObject(PdfIndirectObjectList& objects, PdfName& name, PdfObject*& obj) const;
+        void getExportObject(PdfIndirectObjectList& objects, PdfName& name, PdfObject*& obj) const override;
         bool tryGetCharCode(char32_t codePoint, PdfCharCode& codeUnit) const override;
         bool tryGetCodePoints(const PdfCharCode& codeUnit, std::vector<char32_t>& codePoints) const override;
 
@@ -110,7 +110,7 @@ namespace mm
         PdfWinAnsiEncoding();
 
     protected:
-        void getExportObject(PdfIndirectObjectList& objects, PdfName& name, PdfObject*& obj) const;
+        void getExportObject(PdfIndirectObjectList& objects, PdfName& name, PdfObject*& obj) const override;
         const char32_t* GetToUnicodeTable() const override;
 
     private:
