f50ae7c0b1d581a421256c07d96a8dd7b6be1e60
Francesco Pretto <ceztko@gmail.com>
2022-02-10 23:43:17 +0100

    [CLEAN] Removed unused code

--- test/unit/TestUtils.cpp
+++ test/unit/TestUtils.cpp
@@ -11,21 +11,10 @@
 #include <utfcpp/utf8.h>
 #include <filesystem>
 
-#ifdef _WIN32
-#include <pdfmm/private/WindowsLeanMean.h>
-#elif __linux__
-#include <unistd.h>
-#elif __APPLE__
-#include <mach-o/dyld.h>
-#include <limits.h>
-#endif
-
 #include <TestConfig.h>
 
 using namespace std;
 namespace fs = std::filesystem;
-
-static string GetExecutablePath();
 
 static struct TestPaths
 {
@@ -45,41 +34,3 @@ string TestUtils::GetTestInputFilePath(const string_view& filename)
 {
     return (s_paths.Input / filename).u8string();
 }
-
-#ifdef _WIN32
-
-string GetExecutablePath()
-{
-    wchar_t buf[MAX_PATH];
-    DWORD rc = GetModuleFileNameW(NULL, buf, MAX_PATH);
-    if (rc == 0 || rc == sizeof(buf))
-        throw runtime_error("Can't get module file name");
-
-    return utf8::utf16to8(u16string_view((char16_t*)buf, rc));
-}
-
-#elif __linux__
-
-string GetExecutablePath()
-{
-    char buf[1024] = { 0 };
-    ssize_t size = readlink("/proc/self/exe", buf, sizeof(buf));
-    if (size == 0 || size == sizeof(buf))
-        throw std::runtime_error("Can't read executable path on /proc/self/exe");
-    return std::string(buf, size);
-}
-
-#elif __APPLE__
-
-string GetExecutablePath()
-{
-    string ret[PATH_MAX];
-    uint32_t bufsize = PATH_MAX;
-    if (!_NSGetExecutablePath(buf, &bufsize))
-        throw std::runtime_error("Can't read executable path");
-
-    ret.resize(bufsize);
-    return ret;
-}
-
-#endif
